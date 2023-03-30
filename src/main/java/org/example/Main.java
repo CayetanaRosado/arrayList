1b540cd4837e62195a3480b52e93cc90ee255034
CayetanaRosado <cayetanarosadog@gmail.com>
2023-03-30 10:05:44 +0200

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        //Crea un programa que lea una lista de números enteros desde teclado y
+        // los almacene en un ArrayList. A continuación, el programa deberá mostrar
+        // por pantalla el número de elementos de la lista y la suma de los mismos.
+
+        boolean verdad = true;
+        int numero;
+        int suma = 0;
+        ArrayList<Integer> numerosLista = new ArrayList<Integer>();
+        System.out.println("Escribe los numeros que quieras: ");
+        do {
+            numero = sc.nextInt();
+            if (numero != 0) {
+                numerosLista.add(numero);
+            }
+
+        } while (numero != 0);
+
+        for (int i = 0; i < numerosLista.size(); i++) {
+            suma += numerosLista.get(i);
+        }
+    }
+}
